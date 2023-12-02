99e550a5641f974c5c3a24877958b667cf1e89b8
Wojciech Opara <wwojtek660@gmail.com>
2023-12-02 18:52:02 +0100

    Add help printing

---
+++ Helium/src/help.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+inline void printHelp()
+{
+	// TODO: implement help here
+
+	std::cout << std::endl;
+	std::cout << "Helium" << std::endl;
+	std::cout << "TODO: Implement help page here" << std::endl;
+	std::cout << std::endl;
+}
