44b4f5b45f061ed2fcfdf5a0c8dee38ca852baf0
Wojciech Opara <wwojtek660@gmail.com>
2023-12-18 20:16:41 +0100

    Precompiled headers setup

---
+++ Helium/src/pch/pch.h
@@ -0,0 +1,20 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#pragma once
+
+#include <iostream>
+
+#include <optional>
+#include <variant>
+#include <memory>
+#include <utility>
+
+#include <string>
+#include <string_view>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <map>
+#include <unordered_map>
