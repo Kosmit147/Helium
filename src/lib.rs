7dab2c90f3478101bb1e05591597d09863c92dc8
Wojciech Opara <wwojtek660@gmail.com>
2024-03-25 15:35:02 +0100

    Document config module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
+//! # Helium
+//! A simple compiler written in Rust.
+
 pub mod config;
 
+/// Creates a ```Vec``` of ```String``` elements from string slices.
+///
+/// # Examples
+/// ```
+/// use helium::vec_of_strings;
+///
+/// let names = vec_of_strings!["Mark", "Alice"];
+///
+/// assert_eq!(names[0], String::from("Mark"));
+/// assert_eq!(names[1], String::from("Alice"));
+///
+/// use std::any::Any;
+/// assert!(names.type_id() == Vec::<String>::new().type_id());
+/// ```
 #[macro_export]
 macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
 
+use anyhow::Result;
+
 use helium::config::Config;
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let _config = Config::from_args(&args);
+
+    Ok(())
 }
